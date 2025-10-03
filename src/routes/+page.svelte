9dfe3d36977287e02d6322577ca6978f7c18fb8c
Adhitya Bagus Wicaksono <adhitya.nivoratech.id@gmail.com>
2025-10-03 10:00:38 +0700

    feat: first commit
    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Heading, P, Button } from 'flowbite-svelte';
+</script>
+
+<div
+	class="container mx-auto flex h-[100dvh] flex-col items-center justify-center gap-8 text-center"
+>
+	<Heading tag="h1" class="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+		Nivora Tech ID Boilerplate<br /> with Svelte + Flowbite
+	</Heading>
+
+	<P class="text-center text-lg lg:text-xl xl:px-48 dark:text-gray-400">
+		Ini adalah sebuah cetakan biru yang dapat digunakan dalam lingkungan Nivora Tech ID menggunakan
+		framework Svelte dan Flowbite
+	</P>
+	<Button href="/">Kepercayaan Anda adalah Prioritas Kami</Button>
+</div>
